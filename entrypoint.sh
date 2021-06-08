a041728236fd992af8558dfe07f0869fb8caf0c3
Nico Schottelius <nico@nico-notebook.schottelius.org>
2021-06-08 17:02:01 +0200

    ++shorten the nick

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,10 @@ if [ -z "${NICK}" ]; then
     exit 1
 fi
 
+# Reduce to a maximum of 6 characters and remove -
+# which might be the leading character
+# This leaves some space for the random characters
+NICK=$(echo $NICK | tail -c 7 | sed 's/-//')
+
 sed -i -e "s/MYNAME/$NICK/" /home/eggdrop/eggdrop.conf
 su - eggdrop -c /home/eggdrop/eggdrop.sh
